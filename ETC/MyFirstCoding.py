a961dca171d96cfb8096fddeaf6df6a3e0fdc658
Younghun Kim <hun43576@hufs.ac.kr>
2023-03-04 18:46:02 +0900

    second commit

--- ETC/MyFirstCoding.py
+++ ETC/MyFirstCoding.py
@@ -4,6 +4,10 @@ def main():
     And this python script is a sample for checking,\n
     whether uploading this file to 'github' is working well.
     """)
+    print("""
+    그리고 여기 부분은 초고에서 수정된 내용으로,\n
+    추가한 내용의 업데이트를 확인하기 위한 문구입니다.
+    """)
 
 
 if __name__ == "__main__":
