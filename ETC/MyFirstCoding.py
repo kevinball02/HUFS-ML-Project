9369a5ea62dbd20b9b955090829bf4b30e746e1d
Younghun Kim <hun43576@hufs.ac.kr>
2023-03-04 18:32:57 +0900

    first commit

---
+++ ETC/MyFirstCoding.py
@@ -0,0 +1,10 @@
+def main():
+    print("""
+    This is my first coding.\n
+    And this python script is a sample for checking,\n
+    whether uploading this file to 'github' is working well.
+    """)
+
+
+if __name__ == "__main__":
+    main()
